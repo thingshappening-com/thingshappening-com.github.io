63c12f9db5c4a140157b9b5e34bdcc4562ce5bae
jtburum <jack.t.burum@gmail.com>
2024-04-13 12:42:01 -0400

    rebrand from ttt to  things happeing. initial routing and post setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://tinytechtuts.com",
+  site: "https://thingshappening.com",
   trailingSlash: "ignore",
   build: {
     format: "directory",
