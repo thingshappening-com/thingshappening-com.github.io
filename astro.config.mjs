f32f220cd025f220a5b8e0677325db7715d430f0
jtburum <jack.t.burum@gmail.com>
2024-05-06 19:07:04 -0400

    search attempt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://thingshappening-com.github.io",
+  site: "https://thingshappening.com",
   trailingSlash: "ignore",
   build: {
     format: "directory",
