1f586142d4af8f897d7556b0d45d94a9e4c14d7a
jtburum <jack.t.burum@gmail.com>
2024-05-11 13:45:58 -0400

    update index

--- scripts/search/prepare-index.js
+++ scripts/search/prepare-index.js
@@ -10,8 +10,8 @@ import grayMatter from "gray-matter";
   const srcDir = path.join(process.cwd(), "src");
   const publicDir = path.join(process.cwd(), "public");
   const contentDir = path.join(srcDir, "pages");
-  // let contentFilePattern = path.join(contentDir, "*.md");
   let contentFilePattern = path.join(contentDir, "**", "*.md");
+
   if(os.platform().includes("win")) {
     contentFilePattern = contentFilePattern.replaceAll("\\", "/");
   }
@@ -19,11 +19,23 @@ import grayMatter from "gray-matter";
 
   const contentFilePaths = await globby([contentFilePattern]);
 
-  const getSlugFromPathname = (pathname) =>
-    path.basename(pathname, path.extname(pathname));
+        // 'chattanooga/events/tap-house/2024/trivia.md'
+      // chattanooga/events/tap-house/2024/trivia/2024-05-15
+  const getSlugFromPathname = (pathname, date = null) => {
+    if (date) {
+      // console.log(pathname);
+      pathname = pathname.replace(".md", `/${date}`)
+      console.log(pathname);
+      console.log(date);
+      path.basename(pathname, path.extname(pathname));
+    } else {
+      path.basename(pathname, path.extname(pathname));
+    }
+  }
+    
 
-  console.log("CFPs")
-  console.log(contentFilePaths);
+  // console.log("CFPs")
+  // console.log(contentFilePaths);
   if (contentFilePaths.length) {
     const files = contentFilePaths.map(
       async (filePath) => await fs.readFile(filePath, "utf8")
@@ -32,17 +44,36 @@ import grayMatter from "gray-matter";
     let i = 0;
     for await (let file of files) {
       const {
-        data: { title, description, tags },
+        data: { title, description, tags, eventDates },
         content,
       } = grayMatter(file);
-      index.push({
-        slug: getSlugFromPathname(contentFilePaths[i]),
-        category: "blog",
-        title,
-        description,
-        tags,
-        body: content,
-      });
+
+      // I think this is where the magic needs to happen
+      // 'chattanooga/events/tap-house/2024/trivia.md'
+      // chattanooga/events/tap-house/2024/trivia/2024-05-15
+      if(eventDates) {
+        eventDates.forEach((date) => {
+          index.push({
+            slug: getSlugFromPathname(contentFilePaths[i], date),
+            category: "blog",
+            title,
+            description,
+            tags,
+            body: content,
+          });
+        })
+      } else {
+        console.log("else")
+        index.push({
+          slug: getSlugFromPathname(contentFilePaths[i]),
+          category: "blog",
+          title,
+          description,
+          tags,
+          body: content,
+        });
+      }
+
       i++;
     }
     await fs.writeFile(indexFile, JSON.stringify(index));
