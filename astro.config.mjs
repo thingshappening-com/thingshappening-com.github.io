e98dd04bed7a7c9ce7f90e37107afd6b7d157b7c
jtburum <jack.t.burum@gmail.com>
2025-02-12 22:45:40 -0500

    update spacing and fix tailwind after mdx migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
   site: "https://thingshappening.com",
   trailingSlash: "ignore",
   build: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./public/**/*.html",
-    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md}",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}",
   ],
   important: true,
   variants: {
