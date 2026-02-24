d7bdc1acf6514eb975fa70082c118d721987b489
jtburum <jack.t.burum@gmail.com>
2026-02-24 12:33:25 -0500

    footer spacing

--- dist/_astro/Footer.astro_astro_type_script_index_1_lang.CGQVQ7XB.js
+++
@@ -1,1 +0,0 @@
-import"https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
--- dist/_astro/Footer.astro_astro_type_script_index_3_lang.CMirpMYo.js
+++
@@ -1,1 +0,0 @@
-import"https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/prism.min.js";
